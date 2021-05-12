4bc868a633145c36004f82b7b1ea46bd87b277f2
chiabre <luca.chiabrera@moviri.com>
2021-05-12 13:40:51 -0400

    jmx config to support cpu monitoring

--- build.sh
+++ build.sh
@@ -4,9 +4,10 @@ KONAKART_VERSION="9400_ce"
 JMX_EXPORTER_VERSION="0.15.0"
 
 
-for KONAKART_VERSION in "9400_ce" "9200_ce"
+for KONAKART_VERSION in "9200_ce" "9400_ce"
 do
     docker build --network host --build-arg KONAKART_VERSION=${KONAKART_VERSION} --build-arg JMX_EXPORTER_VERSION=${JMX_EXPORTER_VERSION} -t "chiabre/konakart_jmx_exporter:${KONAKART_VERSION}-${JMX_EXPORTER_VERSION}" .
+    docker push "chiabre/konakart_jmx_exporter:${KONAKART_VERSION}-${JMX_EXPORTER_VERSION}"
 done
 
 
