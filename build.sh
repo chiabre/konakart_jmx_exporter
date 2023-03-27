a89c9349e18020114e93663fe6c4004c5d48779f
chiabre <luca.chiabrera@gmail.com>
2023-03-27 11:25:55 -0400

    konakart 9600, jmx exporter 0.18.0

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 KONAKART_VERSION="9600_ce"
-JMX_EXPORTER_VERSION="0.6.2"
+JMX_EXPORTER_VERSION="0.18.0"
 
 
-docker build --no-cache  --network host --build-arg KONAKART_VERSION=${KONAKART_VERSION} --build-arg JMX_EXPORTER_VERSION=${JMX_EXPORTER_VERSION} -t "chiabre/konakart_jmx_exporter:${KONAKART_VERSION}-${JMX_EXPORTER_VERSION}" .
-docker push "chiabre/konakart_jmx_exporter:${KONAKART_VERSION}-${JMX_EXPORTER_VERSION}"
+docker build --no-cache  --progress=plain --network host --build-arg KONAKART_VERSION=${KONAKART_VERSION} --build-arg JMX_EXPORTER_VERSION=${JMX_EXPORTER_VERSION} -t "chiabre/konakart_jmx_exporter:${KONAKART_VERSION}-${JMX_EXPORTER_VERSION}" .
+#docker push "chiabre/konakart_jmx_exporter:${KONAKART_VERSION}-${JMX_EXPORTER_VERSION}"
 
 
